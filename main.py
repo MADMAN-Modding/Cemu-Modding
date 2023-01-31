05548766971b1dd2298df6336408f05aa9d92d4f
MAD <mad@kmdtech.net>
2023-01-31 17:52:21 -0500

    file copying sorta works

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import os
+
+import shutil
+
 path = input('Please input the folder containing the content folder ')
 
 with os.scandir(path) as it:
     for entry in it:
-        if not entry.name.startswith('.') and entry.is_file or entry.is_folder():
+        if not entry.name.startswith('.') and entry.is_file():
             print(entry.name)
+
+base = input('Base Game Dump ')
+
+with os.scandir(base) as it:
+    for base in it:
+        if not base.name.startswith('.') and base.is_file ():
+            os.stat(entry.name)== os.stat(base.name)
+            shutil.copy(entry.name,base.name)
+        print(base.name)
+            
